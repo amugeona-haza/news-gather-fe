096980d80aa50bfdd044ae8f7f90320e85a5ec2c
eggplantiny <eggplantiny@gmail.com>
2021-11-16 21:24:23 +0900

    sticky header 관련 버그 수정

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,21 +1,24 @@
 <template>
-  <main v-if="show" class="container mx-auto max-w-xl">
-    <app-bar
-      class="sticky top-0 left-0 right-0 bg-white z-10 transition-shadow"
-      :class="isScrolled ? 'shadow-lg' : ''"
-    />
-    <router-view />
-  </main>
-  <div
-    v-else
-    class="w-full h-full flex justify-center items-center"
-  >
-    <div class="flex items-center justify-center space-x-2 animate-bounce">
-      <div class="w-4 h-4 bg-blue-400 rounded-full"></div>
-      <div class="w-4 h-4 bg-green-400 rounded-full"></div>
-      <div class="w-4 h-4 bg-black rounded-full"></div>
+  <template v-if="show">
+    <div class="relative mx-auto max-w-xl">
+      <app-bar
+          class="sticky top-0 left-0 right-0 bg-white z-10 transition-shadow"
+          :class="isScrolled ? 'shadow-lg' : ''"
+      />
+      <main>
+        <router-view />
+      </main>
     </div>
-  </div>
+  </template>
+  <template v-else>
+    <div class="w-full h-full flex justify-center items-center">
+      <div class="flex items-center justify-center space-x-2 animate-bounce">
+        <div class="w-4 h-4 bg-blue-400 rounded-full"></div>
+        <div class="w-4 h-4 bg-green-400 rounded-full"></div>
+        <div class="w-4 h-4 bg-black rounded-full"></div>
+      </div>
+    </div>
+  </template>
 </template>
 
 <script lang="ts" setup>
